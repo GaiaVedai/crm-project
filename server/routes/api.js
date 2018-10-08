a9ec3f3c6f602a51fc9a10322fcc3e0c71bb6767
GaiaVedai <GaiaVedai@gmail.com>
2018-10-08 13:33:33 +0300

    clientBox component

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,15 +3,25 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Client = require('./models/client')
 const data = require('./models/data')
+
 mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost/CRM', function () {
     console.log("DB connection established!!!");
 })
+// add existing data from file
+checkData => () => {
+    collection.count(function (err, count) {
+        if (!err && count === 0) {
+            Client.collection.insertMany(data, function (err, r) {
+                assert.equal(null, err);
+                assert.equal(3, r.insertedCount);
+            
+                db.close();
+            })
+        }
+    });
+}
+//get request for client list - return data
 
-Client.collection.insertMany(data, function (err, r) {
-    assert.equal(null, err);
-    assert.equal(3, r.insertedCount);
 
-    db.close();
-})
 
 module.exports = router
